8aaf06caae03b0a9a019d85cdf84c16cd976c61a
Molly Li <li.molly@hotmail.com>
2024-04-16 22:47:38 -0400

    setup-WarehouseDetails-component-folder

---
+++ src/components/WarehousesDetails/WarehouseDetails.jsx
@@ -0,0 +1,16 @@
+import "./WarehouseDetails.scss"
+
+const WarehouseDetails=()=>{
+
+    return(
+        <>
+        
+        
+        
+        </>
+
+
+    )
+};
+
+export default WarehouseDetails;
