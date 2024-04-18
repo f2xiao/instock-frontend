2ebd381fd426bc338a93e0332d0347cd5d8583ce
Molly Li <li.molly@hotmail.com>
2024-04-17 20:59:45 -0400

    edit-scss-tablet

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -2,30 +2,35 @@ import React from 'react';
 import "./WarehouseDetails.scss";
 import warehouseData from '../../data/01_warehouses.json'; 
 // import { Link } from 'react-router-dom';
+import returnIcon from '../../assets/icons/arrow_back-24px.svg'
 
 const WarehouseDetails = () => { 
     const warehouse = warehouseData[1]; 
     return (
-        <div>
+        <div className="details">
             <div className='details__top'>
-                <h1 className='details__title'>{warehouse.warehouse_name}</h1> 
+                <h1 className='details__title'>
+                    {/* <Link to='/'> */}
+                    <img className='details__return' src={returnIcon} alt="return to warehouse list page" />
+                    {/* </Link> */}
+                    {warehouse.warehouse_name}</h1> 
                 {/* <Link to='/edit'> */}
-                    <div className='details__edit'>EDIT</div>
+                    <button className='details__edit'><span className='details__noshow'>Edit</span></button>
                 {/* </Link> */}
             </div>
-            <div className='detail__bottom'>
-                <div className='detail__address'>
+            <div className='details__bottom'>
+                <div className='details__address'>
                     <h4>WAREHOUSE ADDRESS:</h4>
                     <p>{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</p>
                 </div>
-                <div>
-                    <div className='detail__contact'>
+                <div className='details__contact'>
+                    <div className='details__contacter'>
                         <h4>CONTACT NAME:</h4>
-                        <p>{`${warehouse.contact_name}, ${warehouse.contact_position}`}</p>
+                        <p>{`${warehouse.contact_name},`}<br /> {`${warehouse.contact_position}`}</p>
                     </div>
-                    <div className='detail__info'>
+                    <div className='details__info'>
                         <h4>CONTACT INFORMATION:</h4>
-                        <p>{`${warehouse.contact_phone}, ${warehouse.contact_email}`}</p>
+                        <p>{`${warehouse.contact_phone},`}<br /> {`${warehouse.contact_email}`}</p>
                     </div>
                 </div>
             </div>
