3039a54a11006045047795649e7cfc538f594fdc
Shweta Chavan <chavan.shweta93@gmail.com>
2024-04-19 17:18:20 -0400

    Remove unwanted id

--- src/components/DeleteComponent/DeleteComponent.js
+++ src/components/DeleteComponent/DeleteComponent.js
@@ -4,7 +4,7 @@ import closeIcon from '../../assets/icons/close-24px.svg'
 const DeleteComponent = (props) => {
     return (
         <div className="delete-container" id='delete-modal'>
-            <div className="delete-wrapper" id="modal-wrapper">
+            <div className="delete-wrapper">
                 <div className="delete-wrapper__close-div">
                     <img src={closeIcon} className="delete-wrapper__close-icon" onClick={props.handleCancelClick}></img>
                 </div>
