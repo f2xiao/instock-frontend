97409e9a389c099b95f8f475361b30ffb0caeed3
Charmela Ahilan <charmela6@gmail.com>
2024-04-20 17:56:29 -0400

    revert changes to app.js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.scss";
-import InventoryItemDetails from "./components/InventoryItemDetails/InventoryItemDetails";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        instock
-        <InventoryItemDetails />
-      </div>
+      <div className="App">instock</div>
     </Router>
   );
 }
