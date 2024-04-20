cd46f6007d9236219b8c62a8396e390db9c071b1
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-04-20 01:33:23 -0400

    add route link and edit icon svg in white

--- src/components/DetailsHeader/DetailsHeader.jsx
+++ src/components/DetailsHeader/DetailsHeader.jsx
@@ -1,16 +1,18 @@
 import "./DetailsHeader.scss";
 import backArrowIcon from "../../assets/icons/arrow_back-24px.svg";
-import editIcon from "../../assets/icons/edit-24px.svg";
+import { Link } from "react-router-dom";
 
-const DetailsHeader = ({ title }) => {
+const DetailsHeader = ({ title, linkUrl }) => {
   return (
     <div className="details-header">
       <h1>
-        <img
-          className="details-header__icon"
-          src={backArrowIcon}
-          alt="back arrow icon"
-        />
+        <Link to={linkUrl}>
+          <img
+            className="details-header__icon"
+            src={backArrowIcon}
+            alt="back arrow icon"
+          />
+        </Link>
         {title}
       </h1>
       <button className="details-header__cta details-header__cta--first"></button>
