5314dfb08251f0cfa6947a815b555a4d2854a3f5
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-04-20 01:34:43 -0400

    add WarehouseWetailsPage component

--- src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
+++ src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
@@ -1,7 +1,71 @@
 import "./WarehouseDetailsPage.scss";
+import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
+import DeatilsHeader from "../../components/DetailsHeader/DetailsHeader";
+import Table from "../../components/Table/Table";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { API_URL } from "../../utils/api";
 
 const WarehouseDetailsPage = () => {
-  return <div>WarehouseDetailsPage</div>;
+  const { id } = useParams();
+  const [warehouse, setWarehouse] = useState([]);
+  const [inventorties, setInventories] = useState([]);
+
+  const [isFetching, setIsFetching] = useState(true);
+
+  useEffect(() => {
+    const fetchWarehouse = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/api/warehouses/${id}`);
+        console.log(response.data);
+        setWarehouse(response.data);
+        setIsFetching(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchInventories = async () => {
+      try {
+        const response = await axios.get(
+          `${API_URL}/api/warehouses/${id}/inventories`
+        );
+        console.log(response.data);
+
+        setInventories(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchWarehouse();
+    fetchInventories();
+  }, []);
+
+  if (isFetching) {
+    return (
+      <p className="warehouse-details-page__wrapper">
+        ... Loading warehouse data ...
+      </p>
+    );
+  }
+
+  const headers = ["inventory item", "category", "Status", "Qty"];
+
+  return (
+    <div className="warehouse-details-page">
+      <div className="warehouse-details-page__info">
+        <DeatilsHeader title={warehouse.warehouse_name} linkUrl="/warehouses" />
+        <WarehouseDetails warehouse={warehouse} />
+      </div>
+      <Table
+        type="warehouse-inventories"
+        headers={headers}
+        data={inventorties}
+      />
+    </div>
+  );
 };
 
 export default WarehouseDetailsPage;
