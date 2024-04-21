0c8aaf0b4387fa8f5e201a6bd2f796e3c65c328a
Shweta Chavan <chavan.shweta93@gmail.com>
2024-04-21 18:45:33 -0400

    Add inventory edit and add page

--- src/pages/InventoryEditPage/InventoryEditPage.jsx
+++ src/pages/InventoryEditPage/InventoryEditPage.jsx
@@ -1,111 +1,49 @@
 import "./InventoryEditPage.scss";
 import backArrow from "../../assets/icons/arrow_back-24px.svg"
-import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { API_URL } from "../../utils/api";
+import InventoryForm from "../../components/InventoryForm/InventoryForm";
 
 const InventoryEditPage = () => {
 
   const navigate = useNavigate();
   const { id } = useParams();
-  const [inventoryData, setInventoryData] = useState();
-  const categories = [
-    { label: 'Electronics', value: 'Electronics' },
-    { label: 'Gear', value: 'Gear' },
-    { label: 'Apparel', value: 'Apparel' },
-    { label: 'Accessories', value: 'Accessories' },
-    { label: 'Health', value: 'Health' }];
+  const [warehouseList, setWarehouseList] = useState([]);
 
-  const warehouseList = [
-    { label: 'Manhattan', value: 'Manhattan' },
-    { label: 'Washington', value: 'Washington' },
-    { label: 'Jersey', value: 'Jersey' },
-    { label: 'SF', value: 'SF' },
-    { label: 'Santa Monica', value: 'Santa Monica' },
-    { label: 'Seattle', value: 'Seattle' },
-    { label: 'Miami', value: 'Miami' }];
-  const [value, setValue] = useState('');
-
-  const handleWarehouseChange = (event) => {
-    setValue(event.target.value);
+  const fetchWarehouseList = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/api/warehouses`);
+      if (response.status === 200) {
+        setWarehouseList(response.data);
+      }
+    } catch (error) {
+      console.log('Error while fetch warehouse list');
+    }
   };
 
-  const handleCategoryChange = (event) => {
-    setValue(event.target.value);
-  };
+  useEffect(() => {
+    fetchWarehouseList();
+  }, []);
+
   const handleBackClick = () => {
     navigate('/inventories');
   }
 
-  const fetchInventoryData = async () => {
-    try {
-      const response = await axios.get(`${API_URL}/api/inventories/${id}`);
-      if (response.status === 200) {
-        setInventoryData(response.data);
-      }
-    } catch (error) {
-      console.log(`Failed to get inventory by ${id}`, error);
-    }
-  }
-
-  useEffect(() => {
-    fetchInventoryData();
-  }, [id]);
-
-  if (!inventoryData) {
-    return <div>Loading Inventory Details...</div>
-  }
-
   return (
-    <div>
-      <div>
-        <img src={backArrow} onClick={handleBackClick}></img>
+    <>
+      <h1 className='add__title'>
+        <img className='add__return' src={backArrow} onClick={handleBackClick} alt="return to the main page" />
         Edit Inventory Item
-      </div>
-      <div className="inventory-form">
-          <div>
-            <h2>Item Details</h2>
-            <div className="inventory-form__item-details">
-              <div>
-                <label>Item Name</label><br/>
-                <input value={inventoryData.item_name}></input>
-              </div>
-              <div>
-                <label>Dscription</label><br/>
-                <textarea value={inventoryData.description}></textarea>
-              </div>
-              <div>
-                <label>Category</label><br/>
-                <select value={value} onChange={handleCategoryChange}>
-                  {categories.map((category) => (
-                    <option value={category.value}>{category.label}</option>
-                  ))}</select>
-              </div>
-            </div>
-          </div>
-          <div>
-            Item Availability
-            <div className="inventory-form__item-availability">
-              <div>
-                <label>Status</label><br/>
-                <label><input type='radio' name="status_inStock" value="In Stock" defaultChecked></input>In Stock</label>
-                <label><input type='radio' name="status_outOfStock" value="Out of stock"></input>Out of stock</label>
-              </div>
-              <div>
-                <label>Warehouse</label><br/>
-                <select value={value} onChange={handleWarehouseChange}>
-
-                  {warehouseList.map((warehouse) => (
-
-                    <option value={warehouse.value}>{warehouse.label}</option>
-
-                  ))}</select>
-              </div>
-            </div>
-          </div>
-      </div>
-    </div >
+      </h1>
+      <InventoryForm
+        id={id}
+        action='Edit'
+        warehouseList={warehouseList}
+        handleBackClick={handleBackClick}
+      />
+    </>
   );
 };
 
